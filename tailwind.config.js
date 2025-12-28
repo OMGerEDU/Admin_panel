a4953bb249ccf670ba4cde6f291b665541773b3d
Builders Green <sasha463111@gmail.com>
2025-12-28 12:42:46 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,7 +48,7 @@ export default {
                 sm: "calc(var(--radius) - 4px)",
             },
             fontFamily: {
-                sans: ["Inter", "sans-serif"],
+                sans: ["Assistant", "Inter", "sans-serif"],
             }
         },
     },
